494974bd2cfad2e6418ab26fb4ccf854e7018d22
Randy Davis <randy_davis@outlook.com>
2016-03-19 00:22:10 -0500

    add already completed files from old repo to week1-intro directory for the new repo

---
+++ week1-intro/hello_world_express/app.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+    app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.use(function (req, res) {
+   res.sendStatus(404, 'Not Found');
+});
+
+var server = app.listen(3000, function () {
+    var port = server.address().port;
+    console.log('Express server listening on port %s', port);
+});
+
+// var express = require('express'),
+//     app = express();
+//
+// app.get('/', function(req, res){
+//     res.send('Hello World');
+// });
+//
+// app.use(function(req, res){
+//     res.sendStatus(404);
+// });
+//
+// var server = app.listen(3000, function() {
+//     var port = server.address().port;
+//     console.log('Express server listening on port %s', port);
+// });
